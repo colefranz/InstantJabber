f58a071bcce46249698fb8dcc4b6e97dfd7decc4
Silas <skovats@shaw.ca>
2017-03-23 17:46:38 -0600

    Added error handling to new user page.

--- app/js/directives/loginDirective.js
+++ app/js/directives/loginDirective.js
@@ -14,8 +14,7 @@
             type: 'user',
             name: 'Existing user',
             description: 'Already have an account? Sign in here using your email and password.',
-            button: 'Sign in',
-            firstControl: '#emailTextBox'
+            button: 'Sign in'
           },
           guest: {
             type: 'guest',
@@ -27,8 +26,7 @@
             type: 'create',
             name: 'New user',
             description: 'Don\'t have an account? Creating a new account is quick and simple.',
-            button: 'Sign up',
-            firstControl: '#regEmailTextBox'
+            button: 'Sign up'
           },
           none: {
             type: 'none',
@@ -39,9 +37,14 @@
         };
 
         const errorMessages = {
+          missingName: 'Enter your name.',
           missingEmail: 'Enter your email address.',
           missingPassword: 'Enter your password.',
-          incorrectPassword: 'The email or password is incorrect.'
+          missingPasswordNewAccount: 'Enter a password.',
+          missingConfirmPassword: 'Confirm your password by typing it again.',
+          incorrectPassword: 'The email or password is incorrect.',
+          passwordMismatch: 'The passwords do not match.',
+          emailTaken: 'An account with this email address already exists.'
         };
 
         scope.existingUserForm = {
@@ -49,42 +52,59 @@
           pass: ''
         };
 
+        scope.newUserForm = {
+          name: '',
+          email: '',
+          pass: '',
+          confirmPass: ''
+        };
+
         scope.loginType = loginTypes.none;
         scope.loginTypes = loginTypes;
         
         scope.errors = {
+          name: false,
           email: false,
-          password: false
+          password: false,
+          confirmPassword: false
         };
 
         authService.registerLoginStateObserver(function(isLoggedIn) {
           resetErrors();
 
           if (!isLoggedIn) {
-            scope.errors.email = Error('');
-            scope.errors.password = Error(errorMessages.incorrectPassword);
-            $('#passwordTextBox').focus();
+            if (scope.loginType === scope.loginTypes.user) {
+              scope.errors.email = Error('');
+              scope.errors.password = Error(errorMessages.incorrectPassword);
+              $('#passwordTextBox').focus();
+            } else {
+              scope.errors.email = Error(errorMessages.emailTaken);
+              $('#regEmailTextBox').focus();
+            }
           }
         });
 
         scope.switchLogin = function(type) {
           scope.loginType = type;
           resetErrors();
+          scope.newUserForm.pass = '';
+          scope.newUserForm.confirmPass = '';
+          scope.existingUserForm.pass = '';
         };
 
         scope.handleLoginAttempt = function() {
           var controlToFocus = '';
           resetErrors();
+          scope.existingUserForm.email = scope.existingUserForm.email.trim();
 
-          if (!scope.existingUserForm.email.trim()) {
+          if (!scope.existingUserForm.email) {
             scope.errors.email = Error(errorMessages.missingEmail);
             controlToFocus = '#emailTextBox';
           }
           
           if (!scope.existingUserForm.pass) {
             scope.errors.password = Error(errorMessages.missingPassword);
-            if (!controlToFocus)
-              controlToFocus = '#passwordTextBox';
+            if (!controlToFocus) controlToFocus = '#passwordTextBox';
           }
           
           if (controlToFocus)
@@ -100,10 +120,49 @@
         };
 
         scope.handleCreateAttempt = function() {
+          var controlToFocus = '';
+          resetErrors();
+          
+          scope.newUserForm.name = scope.newUserForm.name.trim();
+          scope.newUserForm.email = scope.newUserForm.email.trim();
+
+          if (!scope.newUserForm.name) {
+            scope.errors.name = Error(errorMessages.missingName);
+            controlToFocus = '#regNameTextBox';
+          }
+
+          if (!scope.newUserForm.email) {
+            scope.errors.email = Error(errorMessages.missingEmail);
+            if (!controlToFocus) controlToFocus = '#regEmailTextBox';
+          }
+
+          if (!scope.newUserForm.pass) {
+            scope.errors.password = Error(errorMessages.missingPasswordNewAccount);
+            if (!controlToFocus) controlToFocus = '#regPasswordTextBox';
+          }
+
+          if (scope.newUserForm.pass && !scope.newUserForm.confirmPass) {
+            scope.errors.confirmPassword = Error(errorMessages.missingConfirmPassword);
+            if (!controlToFocus) controlToFocus = '#regConfirmPasswordTextBox';
+          }
+
+          if ((scope.newUserForm.pass !== '' ||
+            scope.newUserForm.confirmPass !== '') &&
+            scope.newUserForm.pass !== scope.newUserForm.confirmPass) {
+            scope.errors.confirmPassword = Error(errorMessages.passwordMismatch);
+            if (!scope.errors.password) scope.errors.password = Error('');
+            if (!controlToFocus) controlToFocus = '#regConfirmPasswordTextBox';
+          }
+
+          if (controlToFocus !== '') {
+            $(controlToFocus).focus();
+            return;
+          }
+
           authService.create({
-            id: scope.existingUserForm.email,
-            pass: scope.existingUserForm.pass,
-            name: scope.existingUserForm.name
+            id: scope.newUserForm.email,
+            pass: scope.newUserForm.pass,
+            name: scope.newUserForm.name
           });
         };
 
@@ -116,8 +175,10 @@
         }
 
         function resetErrors() {
+          scope.errors.name = false;
           scope.errors.email = false;
           scope.errors.password = false;
+          scope.errors.confirmPassword = false;
         }
       }
     }
